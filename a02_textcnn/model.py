958134685e0735442bc415bbf32c760d8b37a7a3
lpty <13450223174@163.com>
2018-11-26 20:54:41 +0800

    patch

--- a02_textcnn/model.py
+++ a02_textcnn/model.py
@@ -5,13 +5,14 @@ import numpy as np
 class Model:
 
     def __init__(self, sentence_size, class_num, vocab_size, embed_size,
-                 filters):
+                 filters, filter_num, channel_size):
         self.sentence_size = sentence_size
         self.class_num = class_num
         self.vocab_size = vocab_size
         self.embed_size = embed_size
         self.filters = filters
-        self.filter_num = self.filters.__len__()
+        self.filter_num = filter_num
+        self.channel_size = channel_size
 
     def build(self):
         self.initial_params()
@@ -27,11 +28,16 @@ class Model:
 
     def build_forward(self):
         self.sentence = tf.nn.embedding_lookup(self.embed, self.x)
+        self.sentence_expand = tf.expand_dims(self.sentence, -1)
 
     def build_cnn(self):
         for index, filter_size in enumerate(self.filters):
-            with tf.get_variable_scope(f'cnn_pooling_{index}_{filter_size}'):
-                filter = tf.get_variable()
+            with tf.get_variable_scope(f'cnn_pooling_{filter_size}'):
+                filter = tf.get_variable(name=f'filter_size_{filter_size}',
+                                         shape=[filter_size, self.embed_size, self.channel_size, self.filter_num])
+                conv = tf.nn.conv2d(self.sentence_expand, filter, [1, 1, 1, 1], padding='VALID')
+                pool = tf.nn.max_pool(conv)
+                dense = tf.concat(pool)
 
 def _test():
     pass
